14f5cf794748657cce3412a5945ceaff091305ec
aninditatrimulia <aninditatrimulia27@gmail.com>
2026-01-28 15:24:59 +0700

    Fix syntax error in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-<<<<<<< HEAD
-=======
 use Illuminate\Pagination\Paginator;
 
->>>>>>> 9bbe46c7fe869ced2b14e1be7895e5f4f60d0475
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,10 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-<<<<<<< HEAD
-        \Illuminate\Pagination\Paginator::useBootstrapFive();
-=======
         Paginator::useBootstrapFive();
->>>>>>> 9bbe46c7fe869ced2b14e1be7895e5f4f60d0475
     }
 }
